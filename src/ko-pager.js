831188d45650f5e6a0833467696f23f86bac7deb
Bobby Cafazzo <bcafazzo@nbi.company>
2015-12-28 15:01:38 -0500

    Adding sort clicks.

--- src/ko-pager.js
+++ src/ko-pager.js
@@ -6,6 +6,7 @@
  
 var koPager = koPager || {};
 koPager.pagerDefaults = koPager.pagerDefaults || {
+	debug: false,
     selection: false,
     increments: [10, 25, 50, 100],
     init: null,
@@ -38,12 +39,26 @@ ko.components.register('ko-pager', {
         console.log(self.options);
         self.data = ko.observableArray(ko.utils.unwrapObservable(self.options.data || []));
         self.options.fields = (self.options.fields || []).map(function (item) {
-            console.log(item);
             return $.extend({}, koPager.fieldDefaults, item);
         });
-        console.log(self.options.fields);
+		self.text = ko.observable("Test");
+		self.sort = ko.observable(self.options.fields[0].field);
+		self.sortDown = ko.observable(false);
+		self.setSort = function(field){
+			if(field.sortable){
+				if(self.sort() === field.field){
+					self.sortDown(!self.sortDown());
+				}else{
+					self.sort(field.field);
+				}
+			}
+		};
     },
-    template: '<div class="table-responsive">' +
+    template: '<span data-bind="if: options.debug">' +
+		'Sort: <span data-bind="text: sort"></span>' +
+		' - Down: <span data-bind="text: sortDown"></span>' +
+		'</span>' +
+		'<div class="table-responsive">' +
         '<table class="table table-striped">' +
         '<thead>' +
         '<tr data-bind="foreach: options.fields">' +
@@ -53,7 +68,7 @@ ko.components.register('ko-pager', {
         '</thead>' +
         '<tbody data-bind="foreach: data">' +
         '<tr data-bind="foreach: $parent.options.fields">' +
-        '<td data-bind="template: { name: contentTemplate, data: { pager: $parent.$parent, data: $parent, field: $data } }, attr: { class: contentClass, with: $parent }">' +
+        '<td data-bind="template: { name: contentTemplate, data: { pager: $parent, data: $parent, field: $data } }, attr: { class: contentClass, with: $parent }">' +
         '</td>' +
         '</tr>' +
         '</tbody>' +
@@ -63,11 +78,16 @@ ko.components.register('ko-pager', {
 
 $("body").append(
     '<script type="text/html" id="ko-pager-default-header-template">' +
-    '<span data-bind="text: field.title"></span>' +
+	'<span data-bind="click: function() { pager.setSort(field); }">' +
+    '<span data-bind="if: field.sortable">' +
+	'<span data-bind="attr: { class: pager.sort() === field.field ? pager.sortDown() ? pager.options.sortDownIcon : pager.options.sortUpIcon : pager.options.sortNoneIcon }"></span>' +
+	'</span>' +
+	'<span data-bind="text: field.title"></span>' +
+	'</span>' +
     '</script>'
 );
 $("body").append(
     '<script type="text/html" id="ko-pager-default-content-template">' +
-    '<span data-bind="text: data[field.field]"></span>' +
+	'<span data-bind="text: data[field.field]"></span>' +
     '</script>'
 );
